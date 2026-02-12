91e0a80bda354d608fcefa4bfc87836e4fb460c8
amisha-singh12 <singhamisha1572@gmail.com>
2026-02-12 23:16:46 +0530

    hongay aaj ka

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const express= require('express');
 const userRouter = require('./routes/user.routes');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const connectDB = require('./config/db');
+connectDB();
+
+
+
+
 const app= express();
 
 app.set('view engine','ejs');
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
 
 app.use('/user', userRouter);
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,9 +1,33 @@
 const express = require('express');
-
 const router = express.Router();
+
+const { body, validationResult } = require('express-validator');
+
+const UserModel = require('../models/user.model');
 
 router.get('/register', (req, res) => {
     res.render('register');
+})
+
+router.post('/register',
+    body('email').trim().isEmail().isLength({ min: 11 }),
+    body('password').trim().isLength({ min: 4 }),
+    body('username').trim().notEmpty().isLength({ min: 3}),
+    (req, res) => {
+    const errors = validationResult(req);
+    
+    if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array(),
+            message: 'Invalid input'
+         });
+    }
+    const {username, email, password} = req.body;
+    const newUser = UserModel.create({
+        username,
+        email,
+        password
+    })
+     res.json(newUser);
 })
 
 module.exports = router;
